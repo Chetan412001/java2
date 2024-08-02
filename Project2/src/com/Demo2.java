4f7dc219d2605486bef258c0d37e119f3785263c
Chetan Dhotre <Chetan Dhotre@LAPTOP-KNK8AUIO>
2024-08-02 12:00:48 +0530

    project 2

---
+++ Project2/src/com/Demo2.java
@@ -0,0 +1,13 @@
+package com;
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		System.out.println("Demo 2 Project");
+	}
+	
+	public void m1()
+	{
+		System.out.println("m1 method");
+	}
+}
